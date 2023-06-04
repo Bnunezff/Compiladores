30e6fe8546d19abeacea8496df6bd2c79eb3a372
Bnunezff <133238169+Bnunezff@users.noreply.github.com>
2023-06-04 11:19:39 -0400

    Add files via upload

---
+++ AnalizadorLexico/Lexico/src/main/java/com/mycompany/lexico/Lexico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lexico;
+
+/**
+ *
+ * @author braunynunez
+ */
+public class Lexico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
